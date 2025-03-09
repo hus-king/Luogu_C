96e0329e41279bba86259df12ae3e842518642f9
hus-king <3334991525@qq.com>
2025-03-09 17:33:06 +0800

    高精度计算

--- P1009.cpp
+++ P1009.cpp
@@ -1,61 +1,55 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<cstring>
+#include<vector>
 using namespace std;
-
-vector<int> fac[60];
-
-vector<int> multiply(vector<int> a, int b) {
-    vector<int> result;
-    int carry = 0;
-    for (int i = 0; i < a.size(); i++) {
-        int product = a[i] * b + carry;
-        result.push_back(product % 10);
-        carry = product / 10;
+vector<vector<int>> number;
+vector<int> add(vector<int> a,vector<int> b);
+vector <int> output;
+void calculate(int n);
+vector<int> mul(vector<int> a,int b);
+int main(){
+    int n;
+    cin>>n;
+    calculate(n);
+    for(int i=0;i<n;i++){
+        output = add(output,number[i]);
     }
-    while (carry) {
-        result.push_back(carry % 10);
-        carry /= 10;
+    for(int i=output.size()-1;i>=0;i--){
+        cout<<output[i];
+    }
+    return 0;
+}
+void calculate(int n){
+    number.push_back({1});
+    for(int i=2;i<=n;i++){
+        vector<int> temp = mul(number.back(),i);
+        number.push_back(temp);
+    }
+}
+vector<int> mul(vector<int> a,int b){
+    vector <int> result;
+    int carry = 0;
+    for (int i=0;i<a.size();i++){
+        int temp = a[i]*b+carry;
+        result.push_back(temp%10);
+        carry = temp/10;
+    }
+    while(carry){
+        result.push_back(carry%10);
+        carry/=10;
     }
     return result;
 }
-
-vector<int> factorial(int n) {
-    vector<int> ans(1, 1);
-    for (int i = 1; i <= n; i++) {
-        ans = multiply(ans, i);
-        fac[i] = ans;
+vector<int> add(vector<int> a,vector<int> b){
+    vector<int> result;
+    int carry = 0;
+    for(int i=0;i<a.size()||i<b.size();i++){
+        int tempa = i<a.size()?a[i]:0;
+        int tempb = i<b.size()?b[i]:0;
+        int temp = tempa+tempb+carry;
+        result.push_back(temp%10);
+        carry = temp/10;
     }
-    return ans;
-}
-
-void print(vector<int> num) {
-    for (int i = num.size() - 1; i >= 0; i--) {
-        cout << num[i];
-    }
-    cout << endl;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> ans(1, 0);
-    factorial(n);
-    for (int i = 1; i <= n; i++) {
-        vector<int> temp = ans;
-        ans = vector<int>(max(ans.size(), fac[i].size()), 0);
-        int carry = 0;
-        for (int j = 0; j < ans.size(); j++) {
-            if (j < temp.size()) ans[j] += temp[j];
-            if (j < fac[i].size()) ans[j] += fac[i][j];
-            ans[j] += carry;
-            carry = ans[j] / 10;
-            ans[j] %= 10;
-        }
-        while (carry) {
-            ans.push_back(carry % 10);
-            carry /= 10;
-        }
-    }
-    print(ans);
-    return 0;
+    if(carry) result.push_back(carry);
+    return result;
 }
