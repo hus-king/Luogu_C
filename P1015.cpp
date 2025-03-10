7b1da117a062eec74ebf6afab5993c1ba9ca271f
hus-king <3334991525@qq.com>
2025-03-10 19:10:53 +0800

    重构P1015.cpp，简化十六进制位反转和大数相加的逻辑，优化代码结构

--- P1015.cpp
+++ P1015.cpp
@@ -1,84 +1,54 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
-#include <vector>
 using namespace std;
-vector<int> int2vector(int n);
-vector<int> add(vector<int> a,int n);
-unsigned int reverseHexBits(unsigned int num);
+string calculate(string a,int n);
+int check(string a);
 int main(){
     int n;
-    cin>>n;
-    if(n==16){
-        unsigned int m;
-        cin >> hex >> m;
-        cout << hex << m;
-        int flag =0, key = 0;
-        for(int i = 1; i <= 30; i++){
-            key ++;
-            unsigned int temp = reverseHexBits(m);
-            unsigned int temp2 = m + temp;
-            if(temp2 == reverseHexBits(temp2)){
-                cout << key << endl;
-                flag = 1;
-                break;
-            }
-            m = temp2;
+    string m;
+    cin>>n>>m;
+    int step = 1;
+    int key = 0; 
+    while(step <= 30){
+        m = calculate(m,n);
+        if(check(m)){
+            cout<<"STEP="<<step<<endl;
+            key = 1;
+            break;
         }
-        if(flag == 0){
-            cout << "Impossible!" << endl;
-        }
+        step++;
     }
-    else{
-        int m;
-        cin>>m;
-        vector<int> a = int2vector(m);
-        int flag = 0, key = 0;
-        for(int i=1;i<=30;i++){
-            key++;
-            vector<int> temp = add(a,m);
-            vector<int> temp2 = temp;
-            reverse(temp.begin(),temp.end());
-            if(temp2==temp){
-                cout<<key<<endl;
-                flag = 1;
-                break;
-            }
-            a = temp2;
-        }
-        if(flag==0){
-            cout<<"Impossible!"<<endl;
-        }
-    }
+    if(!key) cout<<"Impossible!"<<endl;
 }
-vector<int> int2vector(int n){
-    vector<int> result;
-    while(n){
-        result.push_back(n%10);
-        n/=10;
-    }
-    return result;
-}
-vector<int> add(vector<int> a,int n){
-    vector<int> b = a;
+string calculate(string a,int n){
+    string b = a;
     reverse(b.begin(),b.end());
-    vector<int> result;
+    string reslut;
     int carry = 0;
-    for(int i=0;i<b.size()||i<a.size();i++){
-        int tempa = i<a.size()?a[i]:0;
-        int tempb = i<b.size()?b[i]:0;
-        int temp = tempa+tempb+carry;
-        result.push_back(temp%n);
-        carry = temp/n;
+    for(int i=0;i < a.size() || i < b.size();i++){
+        int tempa,tempb;
+        if (n==16){
+            tempa = i < a.size() ? a[i] > '9' ? a[i] - 'A' + 10 : a[i] - '0' : 0;
+            tempb = i < b.size() ? b[i] > '9' ? b[i] - 'A' + 10 : b[i] - '0' : 0;
+        }
+        else{
+            tempa = i < a.size() ? a[i] - '0' : 0;
+            tempb = i < b.size() ? b[i] - '0' : 0;
+        }
+        int temp = tempa + tempb + carry;
+        char c;
+        if(n == 16) c = temp % 16 > 9 ? temp % 16 - 10 + 'A' : temp % 16 + '0';
+        else c = temp % n + '0';
+        reslut.push_back(c);
+        carry = temp / n;
     }
-    if(carry) result.push_back(carry);
-    return result;
+    if(carry) reslut.push_back(carry + '0');
+    return reslut;
 }
-unsigned int reverseHexBits(unsigned int num) {
-    unsigned int reversed = 0;
-    const int bits = sizeof(num) * 8; // 获取总位数（如32位）
-    for (int i = 0; i < bits; i += 4) {
-        reversed <<= 4;          // 左移4位腾出空间
-        reversed |= (num >> i) & 0xF; // 提取当前半字节并组合
-    }
-    return reversed;
+int check(string a){
+    string b = a;
+    reverse(b.begin(),b.end());
+    if(a == b) return 1;
+    else return 0;
 }
