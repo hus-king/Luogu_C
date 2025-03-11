de01933632782d3dcadf793c68d30c801e4f961f
3334991525 <3334991525@qq.com>
2025-03-11 12:40:06 +0000

    重构P1048.cpp，优化背包问题动态规划算法，简化数据结构

--- P1048.cpp
+++ P1048.cpp
@@ -4,25 +4,19 @@ using namespace std;
 int main(){
     int t,m;
     cin>>t>>m;
-    vector<vector<int>> v;
+    vector<int> time;
+    vector<int> value;
     for(int i=0;i<m;i++){
         int a,b;
         cin>>a>>b;
-        vector<int> temp;
-        temp.push_back(a);
-        temp.push_back(b);
-        v.push_back(temp);
+        time.push_back(a);
+        value.push_back(b);
     }
-    vector<vector<int>> dp(m+1,vector<int>(t+1,0));
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=t;j++){
-            if(j>=v[i-1][0]){
-                dp[i][j] = max(dp[i-1][j],dp[i-1][j-v[i-1][0]]+v[i-1][1]);
-            }
-            else{
-                dp[i][j] = dp[i-1][j];
-            }
+    int dp[1010]={0};
+    for(int i=0;i<m;i++){
+        for(int j=t;j>=time[i];j--){
+            dp[j]=max(dp[j],dp[j-time[i]]+value[i]);
         }
     }
-
+    cout<<dp[t];
 }
