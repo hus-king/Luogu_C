d79b5c8d7f73ea815efc429cd85be9c595e893b3
hus-king <3334991525@qq.com>
2025-03-17 23:09:58 +0800

    删除 P2574.md 和 P3372.md 文件，新增 P2574_XOR的艺术_线段树.md 和 P3372_线段树模板.md 文件，更新 P2574_2.cpp 文件以实现线段树相关功能

--- P2574_2.cpp
+++ P2574_2.cpp
@@ -1,27 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
+ll n, a[100005], d[270000], b[270000]; 
+// n:数组长度 a:原数组 d:线段树数组 b:懒标记数组
+
+void build(ll l,ll r,ll p){  // l:区间左端点 r:区间右端点 p:节点标号
+    if(l==r){
+        d[p]=a[l];
+        return;
+    }
+    ll m = l + (r-l)/2;
+    build(l,m,p*2);
+    build(m+1,r,p*2+1);
+    d[p]=d[p*2]+d[p*2+1];
+}
+
+void update(ll l,ll r,ll c,ll s,ll t,ll p){  // l:区间左端点 r:区间右端点 c:增加的值 s:修改的区间左端点 t:修改的区间右端点 p:节点标号
+    if(l<=s && t<=r){
+        d[p]+=(t-s+1)*c;
+        b[p]+=c;
+        return;
+    }
+    ll m = s + (t-s)/2;
+    if(b[p]){
+        d[p*2]+=(m-s+1)*b[p];
+        d[p*2+1]+=(t-m)*b[p];
+        b[p*2]+=b[p];
+        b[p*2+1]+=b[p];
+        b[p]=0;
+    }
+    if(l<=m) update(l,r,c,s,m,p*2);
+    if(r>m) update(l,r,c,m+1,t,p*2+1);
+    d[p]=d[p*2]+d[p*2+1];
+}
+
+ll getsum(ll l,ll r,ll s,ll t,ll p){ // l:区间左端点 r:区间右端点 s:查询的区间左端点 t:查询的区间右端点 p:节点标号  
+    if(l<=s && t<=r) return d[p];
+    ll m = s + (t-s)/2;
+    if(b[p]){
+        d[p*2]+=(m-s+1)*b[p];
+        d[p*2+1]+=(t-m)*b[p];
+        b[p*2]+=b[p];
+        b[p*2+1]+=b[p];
+        b[p]=0;
+    }
+    ll sum=0;
+    if(l<=m) sum+=getsum(l,r,s,m,p*2);
+    if(r>m) sum+=getsum(l,r,m+1,t,p*2+1);
+    return sum;
+}
 int main(){
-    int n,m;
-    cin>>n>>m;
-    int num[n+1];
-    int sum[n+1];
-    sum[0]=0;
-    string s;
-    cin>>s;
-    for(int i=1;i<=n;i++){
-        num[i]=s[i-1]-'0';
-        sum[i]=sum[i-1]+num[i];
-    }
-    for(int i=0;i<m;i++){
-        int op,l,r;
-        cin>>op>>l>>r;
-        if(op){
-            int temp;
-            temp = sum[r] - sum[l-1];
-        }
-        else{
-            int index = r - l + 1;
-            int change = index - (sum[r] - sum[l-1]);
+    ll q,i1,i2,i3,i4;
+    cin>>n>>q;
+    for(ll i=1;i<=n;i++) cin>>a[i];
+    build(1,n,1);
+    while(q--){
+        cin>>i1>>i2>>i3;
+        if(i1==2){
+            cout<<getsum(i2,i3,1,n,1)<<endl;
+        }else{
+            cin>>i4;
+            update(i2,i3,i4,1,n,1);
         }
     }
+    return 0;
 }
