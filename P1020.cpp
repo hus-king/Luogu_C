16e691d1ef948ef045d16650e4c23f90ae4fb38c
hus-king <3334991525@qq.com>
2025-04-07 23:38:08 +0800

    优化 P1020.cpp 中的最长递减和递增子序列算法，移除不必要的变量和注释

--- P1020.cpp
+++ P1020.cpp
@@ -8,17 +8,11 @@ int main(){
     }
     int l = a.size();
     vector<int> dp(l+1, 1); // dp[i]表示i之后的最长递减子序列的长度
-    vector<int> maxdp(l+1, 1);// maxdp[i]表示dp[i]到dp[l]的最大值 
     int maxn=dp[l];
-    //i     j      l
     for(int i = l-1; i >= 0; i--){
         for(int j = i+1; j < l; j++){
             if(a[i] >= a[j]){
                 dp[i] = max(dp[i], dp[j]+1);
-                // if(dp[j] == maxdp[j]) break;
-                if(dp[j]+1 > dp[i]){
-                    if(dp[j] == maxdp[j]) break;
-                }
             }
         }
         if(dp[i] > maxn){
@@ -28,17 +22,11 @@ int main(){
     }
     cout << maxn << endl;
     vector<int> dpm(l+1, 1); // dpm[i]表示i之前的最长递增子序列的长度
-    vector<int> maxdpm(l+1, 1);// maxdpm[i]表示dpm[1]到dpm[i]的最大值
-    //j     i      l
     int maxm=dpm[1];
     for(int i = 1; i < l; i++){
         for(int j = 0; j < i; j++){
             if(a[i] > a[j]){
                 dpm[i] = max(dpm[i], dpm[j]+1);
-                // if(dpm[j] == maxdpm[j]) break;
-                if(dpm[j]+1 > dpm[i]){
-                    if(dpm[j] == maxdpm[j]) break;
-                }
             }
         }
         if(dpm[i] > maxm){
