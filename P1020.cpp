7d012f37cdcaa8aef11b058be2a78cfee760c2e4
hus-king <3334991525@qq.com>
2025-04-16 10:56:30 +0800

    重构 P1020.cpp，优化最长递增和非递增子序列的计算逻辑；新增 P1873_砍树.md 文件，添加题目描述及输入输出格式

--- P1020.cpp
+++ P1020.cpp
@@ -1,37 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     vector<int> a;
     int temp;
-    while(cin >> temp){
+    while (cin >> temp) {
         a.push_back(temp);
     }
-    int l = a.size();
-    vector<int> dp(l+1, 1); // dp[i]表示i之后的最长递减子序列的长度
-    int maxn=dp[l];
-    for(int i = l-1; i >= 0; i--){
-        for(int j = i+1; j < l; j++){
-            if(a[i] >= a[j]){
-                dp[i] = max(dp[i], dp[j]+1);
-            }
-        }
-        if(dp[i] > maxn){
-            maxn = dp[i];
-        }
-        maxdp[i] = max(maxdp[i+1], dp[i]);
-    }
-    cout << maxn << endl;
-    vector<int> dpm(l+1, 1); // dpm[i]表示i之前的最长递增子序列的长度
-    int maxm=dpm[1];
-    for(int i = 1; i < l; i++){
-        for(int j = 0; j < i; j++){
-            if(a[i] > a[j]){
-                dpm[i] = max(dpm[i], dpm[j]+1);
-            }
-        }
-        if(dpm[i] > maxm){
-            maxm = dpm[i];
+    int n = a.size();
+    
+    // 处理最长非递增子序列
+    vector<int> dp1;
+    for (int x : a) {
+        // 使用upper_bound在非递增数组中找插入位置
+        auto it = upper_bound(dp1.begin(), dp1.end(), x, greater<int>());
+        if (it == dp1.end()) {
+            dp1.push_back(x);
+        } else {
+            *it = x;
         }
     }
-    cout << maxm << endl;
+    int res1 = dp1.size();
+    
+    // 处理最长递增子序列
+    vector<int> dp2;
+    for (int x : a) {
+        // 使用lower_bound在递增数组中找插入位置
+        auto it = lower_bound(dp2.begin(), dp2.end(), x);
+        if (it == dp2.end()) {
+            dp2.push_back(x);
+        } else {
+            *it = x;
+        }
+    }
+    int res2 = dp2.size();
+    
+    cout << res1 << endl << res2 << endl;
+    return 0;
 }
